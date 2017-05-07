c2d766b0c67b1c406192812b2c0727463f3b33f2
Amontep <morleng@gmail.com>
2017-05-07 20:16:15 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(replyToken, text )
+  }
+  res.sendStatus(200)
+})
+
+function sendText (replyToken, text) {
+  let data = {
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราชื่อ Mink ยินดีที่ได้รู้จัก 🤖'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {Channel Access Token}'
+    },
+    url: 'https://api.line.me/v2/bot/message/reply',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
+}
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
