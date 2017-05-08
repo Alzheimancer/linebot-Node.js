e11d06fc7ac296009f5b52cc9d9c75b990b397a3
Amontep <morleng@gmail.com>
2017-05-09 00:44:55 +0700

    change to push command

--- app.js
+++ app.js
@@ -11,32 +11,32 @@ app.post('/webhook', (req, res) => {
   var text = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
-  
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(replyToken, text )
+    sendText(sender, replyToken, text )
   }
   res.sendStatus(200)
 })
 
-function sendText (replyToken, text) {
+function sendText (sender, replyToken, text) {
   let data = {
+    to: sender,
     replyToken: replyToken,
     messages: [
       {
         type: 'text',
-        text: 'สวัสดีค่ะ เราชื่อ Mink🤖 ยินดีที่ได้รู้จัก '
+        text: 'สวัสดีค่ะ เราชื่อ Mink ยินดีที่ได้รู้จัก 🤖'
       }
     ]
   }
   request({
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer {Channel Access Token}'
+      'Authorization': 'Bearer DbyRCCbfACI2HkGCppFNVRPEuyhtxddgHtC8oJxiddXyfouVzQ1IHU8EKSp1im/xEUxHhmb/wEw+cGYqvCucVUhRmsOQj7tjo1eSZNzYYMzCr6e7/6Y0AKyx5A9gXOtiK0AgfOD6WMMmNi4yjp+11QdB04t89/1O/w1cDnyilFU='
     },
-    url: 'https://api.line.me/v2/bot/message/reply',
+    url: 'https://api.line.me/v2/bot/message/push',
     method: 'POST',
     body: data,
     json: true
